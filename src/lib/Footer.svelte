fbac3e16f53f0a259f2e4d449866e815cbc5a0bd
laixi74 <78567259+laixi74@users.noreply.github.com>
2021-03-23 12:29:11 +1300

    adding bulma
    styled the header and footer using bulma and tested on different browsers.

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,9 @@
 
 
-<footer>
-    <p>&copy; kelly lai 2021</p>
-</footer>
+<footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        &copy; kelly lai 2021
+      </p>
+    </div>
+  </footer>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,5 @@
 
-  <section class="hero">
+  <section class="hero is-link">
     <div class="hero-body">
       <p class="title">
         Hi, my name is Kelly!
