f9bc331060214f74819cb560074c717eea66d96a
laixi74 <78567259+laixi74@users.noreply.github.com>
2021-03-23 12:39:18 +1300

    style header
    changed the style and made it larger

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,12 @@
 
-  <section class="hero is-link">
-    <div class="hero-body">
-      <p class="title">
-        Hi, my name is Kelly!
-      </p>
-      <p class="subtitle">
-        Year 12
-      </p>
-    </div>
-  </section>
+
+<section class="hero is-medium is-link">
+  <div class="hero-body">
+    <p class="title is-size-1">
+      Hi, my name is Kelly!
+    </p>
+    <p class="subtitle is-size-2">
+      Year 12
+    </p>
+  </div>
+</section>
